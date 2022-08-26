4b9e244f56d09328d89dd888b5350b0087e17e7a
negativeh0ll0w <sarahking573@gmail.com>
2022-08-26 10:44:12 -0400

    Add event details modal

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -4,8 +4,19 @@ import { GlobalContext } from '../../context/GlobalState';
 const Event = ({ item }) => {
     const { deleteEvent } = useContext(GlobalContext);
 
+    // modal for event details
+    const toggleModal = (item, i) => {
+        setCurrentEvent({ ...item, index: i });
+        setIsModalOpen(!isModalOpen);
+      };
+
+    // event list view  
     return (
-        <li>
+        <li onClick={() => toggleModal(item, i)}>
+            {isModalOpen && (
+                <EventDetails onClose={toggleModal} currentEvent={currentEvent} />
+            )}
+            <span className="Category">{item.category}</span>
             <span className="Event">{item.event}</span>
             <span className="Time">{item.time}</span>
             <span className="Action" onClick={() => deleteEvent(item.id)}>&#10007;</span>
