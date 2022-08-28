89e15b71a9c5b6cb6965073c6bf616b5b929a7d9
Chrisp1993 <chrispollio93@gmail.com>
2022-08-27 22:32:29 -0400

    started styling the header by creating the navbar.css and importing it. Also commented out the event.js temporarily because it was causing an error (we will fix that)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
+import Navbar from "./components/Header/Navbar";
+
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
     <GlobalProvider>
+      <Navbar />
       <Main />
     </GlobalProvider>
   );
