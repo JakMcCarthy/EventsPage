901bda1f321946db5f9e1e81f3bd86e352ce14d9
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-08-28 01:49:09 -0400

    some login function ground work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Main from './components/Main';
 import { GlobalProvider } from './context/GlobalState';
+// this is Aaron's testing element
+import Test from './components/Test/TestApi';
+// import LoginForm from './components/Login/Login';
 
 function App() {
   return (
     <GlobalProvider>
-      <Main />
+      {/* <LoginForm/> */}
+      {/* <Main /> */}
+      <Test />
     </GlobalProvider>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,47 +1,52 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from 'react';
 import '../../index.css';
-import './login.css';
-import { GlobalContext } from '../../context/GlobalState';
+import './Login.css';
+// import { GlobalContext } from '../../context/GlobalState';
+const { loginHandler,signUpHandler } = require('./userChangeFunctions');
 
 const LoginForm = () => {
+  // function handleSubmit(e){
+  //     e.preventDefault();
+  //     console.log(e.target[1].value);
+  // }
 
-    return (
+  return (
+    <div>
+      <form className='login-forms' id='login-form' onSubmit={loginHandler}>
+        <h2>Log In</h2>
         <div>
-            <form class="login-forms" id="login-form">
-                <h2>Log In</h2>
-                <div>
-                    <label for="email-login">email:</label>
-                    <input type="text" id="email-login" />
-                </div>
-                <div>
-                    <label for="password-login">password:</label>
-                    <input type="password" id="password-login" />
-                </div>
-                <div>
-                    <button type="submit">Log Me In!</button>
-                </div>
-            </form>
-
-            <form class="login-forms" id="signup-form">
-                <h2>Sign Up</h2>
-                <div>
-                    <label for="username-signup">username:</label>
-                    <input type="text" id="username-signup" />
-                </div>
-                <div>
-                    <label for="email-signup">email:</label>
-                    <input type="text" id="email-signup" />
-                </div>
-                <div>
-                    <label for="password-signup">password:</label>
-                    <input type="password" id="password-signup" />
-                </div>
-                <div>
-                    <button type="submit">Sign Me Up!</button>
-                </div>
-            </form>
+          <label for='user-login'>user:</label>
+          <input type='text' id='user-login' />
         </div>
-    );
-}
+        <div>
+          <label for='password-login'>password:</label>
+          <input type='password' id='password-login' />
+        </div>
+        <div>
+          <button type='submit'>Log Me In!</button>
+        </div>
+      </form>
 
-export default LoginPage;
+      <form className='login-forms' id='signup-form' onSubmit={signUpHandler}>
+        <h2>Sign Up</h2>
+        <div>
+          <label for='username-signup'>username:</label>
+          <input type='text' id='username-signup' />
+        </div>
+        <div>
+          <label for='email-signup'>email:</label>
+          <input type='text' id='email-signup' />
+        </div>
+        <div>
+          <label for='password-signup'>password:</label>
+          <input type='password' id='password-signup' />
+        </div>
+        <div>
+          <button type='submit'>Sign Me Up!</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
