54b1b128877d6cbec544fac6c0c4e2b75ae466ea
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-08-29 16:05:40 -0400

    add styling to footer background and flex

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '/Footer.css';
 
 function Footer() {
   return (
     <div>className='footer-container'
-        
+
     </div>
   )
 }
