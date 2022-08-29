16ba7f021d522f11a101deeead7750521f775885
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-08-29 16:46:31 -0400

    add contact us links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '/Footer.css';
 
 function Footer() {
-  return (
-    <div>className='footer-container'>
-        <div className='footer-links'>
-            <div className='footer-link-wrapper'>
-              <div className='footer-link-items'>
-                  <h2>About Us</h2>
-                  <Link to='/'>Careers</Link> 
-                  <Link to='/'>Testimonials</Link>
-                  <Link to='/'>Investors</Link>
-              </div>
+    return (
+        <div>
+            className='footer-container'>
+            <div className='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Investors</Link>
+                    </div>
+                    <div className='footer-link-items'>
+                        <h2>Contace Us</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Footer
+export default Footer;
