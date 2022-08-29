fbc4ef57c3d7fbecaa502e69c1a427b1764f48e1
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-08-29 19:52:28 -0400

    changed import pages path

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import Main from './components/Main';
-import Navbar from "./components/Header/Navbar";
-import Home from "./components/Header/pages/Home"
-import Tickets from "./components/Header/pages/Tickets"
-import Event from "./components/Event/Event"
-import Login from "./components/Header/pages/Login"
-import Signup from "./components/Header/pages/Signup"
+import Navbar from './components/Header/Navbar';
+import Home from './components/pages/Home';
+import Tickets from './components/pages/Tickets';
+import Event from './components/Event/Event';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
 import Footer from '../Footer';
 // these two components allow us to define all routes and group them together
-import { Route, Routes } from "react-router-dom"
+import {Route, Routes} from 'react-router-dom';
 
-import { GlobalProvider } from './context/GlobalState';
+import {GlobalProvider} from './context/GlobalState';
 // this is Aaron's testing element
 // import Test from './components/Test/TestApi';
 
 function App() {
-  return (
-    <GlobalProvider>
-      <Navbar />
-      <div className = "container">
-        <Routes>
-          <Route path ="/" element={<Home />} />
-          <Route path ="/tickets" element={<Tickets />} />
-          <Route path ="/event" element={<Event />} />
-          <Route path ="/login" element={<Login />} />
-          <Route path ="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-      <Main />
-      {/* <Test /> */}
-      <Footer />
-    </GlobalProvider>
-  );
+    return (
+        <GlobalProvider>
+            <Navbar />
+            <div className='container'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/tickets' element={<Tickets />} />
+                    <Route path='/event' element={<Event />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<Signup />} />
+                </Routes>
+            </div>
+            <Main />
+            <Footer />
+            {/* <Test /> */}
+        </GlobalProvider>
+    );
 }
 
 export default App;
