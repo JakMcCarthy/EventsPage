2f91f838f4b1b968637e050e657c6baf3f601e78
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-08-29 16:33:20 -0400

    footer About Us links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '/Footer.css';
 
 function Footer() {
   return (
-    <div>className='footer-container'
-
+    <div>className='footer-container'>
+        <div className='footer-links'>
+            <div className='footer-link-wrapper'>
+              <div className='footer-link-items'>
+                  <h2>About Us</h2>
+                  <Link to='/'>Careers</Link> 
+                  <Link to='/'>Testimonials</Link>
+                  <Link to='/'>Investors</Link>
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
