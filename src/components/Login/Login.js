c535555d39ea0119e75eccac5fdff21f7e92cf59
negativeh0ll0w <sarahking573@gmail.com>
2022-08-29 19:36:12 -0400

    fix login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,30 +4,7 @@ import './Login.css';
 import { GlobalContext } from '../../context/GlobalState';
 const { loginHandler,signUpHandler } = require('./userChangeFunctions');
 
-function LoginForm(props) {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error }] = useMutation(LOGIN);
-  
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const mutationResponse = await login({
-          variables: { email: formState.email, password: formState.password },
-        });
-        const token = mutationResponse.data.login.token;
-        Auth.login(token);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-  
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-      setFormState({
-        ...formState,
-        [name]: value,
-      });
-    };
+const LoginForm = () => {
 
   return (
     <div>
