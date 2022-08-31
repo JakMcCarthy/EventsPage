cdf42f0cbb310a1d60fca55e8d63011dfa0dfd16
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-08-31 12:55:48 -0400

    redirect functional

--- client/src/components/pages/Event.js
+++ client/src/components/pages/Event.js
@@ -1,7 +1,12 @@
 import AddEvent from '../AddEvent/AddEvent';
+import auth from '../../utils/auth';
 
 
 export default function Event() {
+    
+  if (!auth.loggedIn()){
+    window.location.assign('/login');
+  }
   return (
     <>
       <h1>Create Event</h1>
