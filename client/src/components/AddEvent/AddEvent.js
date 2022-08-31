fcca80b54236f19ee507388b4a4a2687c2b92265
negativeh0ll0w <sarahking573@gmail.com>
2022-08-31 17:39:27 -0400

    addevent fix description input box

--- client/src/components/AddEvent/AddEvent.js
+++ client/src/components/AddEvent/AddEvent.js
@@ -45,7 +45,7 @@ const AddEvent = () => {
                         <input className="eventField" value={event} type="text" onChange={(e) => setEvent(e.target.value)} placeholder="New Event" />
                         <input className="eventTime" value={time} type="text" onChange={(e) => setTime(e.target.value)} placeholder="Time" />
                         <br/>
-                        <textarea className="eventDescription" value={description} type="text" onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
+                        <input className="eventDescription" value={description} type="text" onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
                         <br/>
                         <button className="submitBtn">Create Event</button>
                     </form>
