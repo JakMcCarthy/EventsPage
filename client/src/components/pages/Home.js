26c7d7da1073d11af4111d384dfdf4682557859f
negativeh0ll0w <sarahking573@gmail.com>
2022-08-31 07:47:10 -0400

    add addevent to home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 import '../../index.css';
 import HeroSection from '../HeroSection/heroSection';
 import Cards from '../Cards/Cards';
-
+import AddEvent from '../AddEvent/AddEvent';
+import { GlobalProvider } from '../../context/GlobalState';
 
 function Home() {
     return (
         <>
         <HeroSection />
+        { /* only show AddEvent if logged in */ }
+        <AddEvent />
         <Cards />
         </>
     );
